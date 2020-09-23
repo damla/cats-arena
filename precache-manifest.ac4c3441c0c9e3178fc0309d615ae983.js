ffd3cee4d19e7b67a98a1d0f4883dbce9352c03f
Damla Koksal <damlakoksal@outlook.com>
2020-09-23 18:21:32 +0300

    Updates

---
+++ precache-manifest.ac4c3441c0c9e3178fc0309d615ae983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c96794cb217e987d29f1d81e554ba8",
+    "url": "/cats-arena/index.html"
+  },
+  {
+    "revision": "db2e59c8b7c5b34554d2",
+    "url": "/cats-arena/static/css/main.88b8d4ed.chunk.css"
+  },
+  {
+    "revision": "ab3ed9d491e25b27a1df",
+    "url": "/cats-arena/static/js/2.26c82c0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-arena/static/js/2.26c82c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2e59c8b7c5b34554d2",
+    "url": "/cats-arena/static/js/main.4e8170d7.chunk.js"
+  },
+  {
+    "revision": "bb6a1c6a366e81addc7a",
+    "url": "/cats-arena/static/js/runtime-main.48b547d7.js"
+  }
+]);
